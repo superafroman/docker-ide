ee240514f037f01df2e60402b849dbd2fc8c15ca
superafroman <max.stewart@superafroman.com>
2015-02-21 16:57:20 +0000

    Autocomplete instructions.

--- app/scripts/editor/mode/dockerfile-hint.js
+++ app/scripts/editor/mode/dockerfile-hint.js
@@ -5,11 +5,25 @@ angular.module('dockerIde')
     'DOCKER_INSTRUCTIONS',
     function(instructions) {
 
-      function getCompletions() {
-        return instructions;
+      function getCompletions(token, context) {
+
+        if (context.length) {
+          return null;
+        }
+
+        var textRegExp = new RegExp('^' + token.string, 'i'),
+            matchingInstructions = instructions.filter(function(instruction) {
+              return textRegExp.test(instruction);
+            }),
+            uppercase = token.string.charAt(0) === token.string.charAt(0).toUpperCase();
+
+        for (var i = 0; i < matchingInstructions.length; i++) {
+          matchingInstructions[i] = uppercase ? matchingInstructions[i].toUpperCase() : matchingInstructions[i].toLowerCase();
+        }
+        return matchingInstructions;
       }
 
-      function hint(editor, options) {
+      function hint(editor) {
 
         var cursor = editor.getCursor(),
             token = editor.getTokenAt(cursor);
@@ -21,8 +35,17 @@ angular.module('dockerIde')
           token.string = token.string.slice(0, cursor.ch - token.start);
         }
 
+        var previousToken = token,
+            context = [];
+        while (previousToken && previousToken.type === 'argument') {
+          previousToken = editor.getTokenAt(CodeMirror.Pos(cursor.line, previousToken.start));
+          if (previousToken && previousToken.type) {
+            context.push(previousToken);
+          }
+        }
+
         return {
-          list: getCompletions(token, options),
+          list: getCompletions(token, context),
           from: CodeMirror.Pos(cursor.line, token.start),
           to: CodeMirror.Pos(cursor.line, token.end)
         };
--- app/scripts/editor/mode/dockerfile-mode.js
+++ app/scripts/editor/mode/dockerfile-mode.js
@@ -12,15 +12,15 @@ app.run([
   'DOCKER_INSTRUCTIONS',
   function(instructions) {
 
-    var instructionsRegExp = new RegExp('(' + instructions.join('|') + ')(\\s|$)', 'i');
+    var instructionsRegExp = new RegExp('\\s*(' + instructions.join('|') + ')(\\s|$)', 'i');
 
     CodeMirror.defineSimpleMode('dockerfile', {
       start: [
-        { regex: instructionsRegExp, token: [ 'instruction', null ] },
+        { regex: instructionsRegExp, token: [ 'instruction', null ], dedent: true },
         { regex: /"(?:[^\\]|\\.)*?"/, token: 'string' },
         { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: 'number' },
         { regex: /#.*$/, token: 'comment' },
-        { regex: /\\(\s)*$/ }, // TODO: { indent: true } works but dedent doesn't seem to
+        { regex: /\\(\s)*$/, indent: true },
         { regex: /[^\s]+/, token: 'argument'}
       ],
       meta: {
